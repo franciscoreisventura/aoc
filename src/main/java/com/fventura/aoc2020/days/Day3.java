407be615a4ee603d1947dc568dec11542e2bc366
Francisco Ventura <fraven@signicat.com>
2020-12-03 12:22:27 +0000

    ocd

--- src/main/java/com/fventura/aoc2020/days/Day3.java
+++ src/main/java/com/fventura/aoc2020/days/Day3.java
@@ -11,36 +11,32 @@ public class Day3 implements Day {
     @Override
     public Object part1() throws IOException {
         final String[] ride = loadDayStrings(3);
-
         return crashes(ride, 3,1);
     }
 
     @Override
     public Object part2() throws IOException {
         final String[] ride = loadDayStrings(3);
-
         return crashes(ride, 1,1)
                 * crashes(ride, 3, 1)
                 * crashes(ride, 5, 1)
                 * crashes(ride, 7, 1)
                 * crashes(ride, 1, 2);
-
     }
 
     private int crashes(final String[] ride, final int right, final int down) {
         int crashes = 0;
-
-        for(int i=0,j=0; i<ride.length; i = i + down,j = j + right) {
+        for (int i = 0, j = 0; i < ride.length; i = i + down,j = j + right) {
             final String slope = ride[i];
-            if(hit(slope,j)) {
+            if (hitTree(slope,j)) {
                 crashes++;
             }
         }
         return crashes;
     }
 
-    private boolean hit(final String slope, int j) {
-        if(j >= slope.length()) {
+    private boolean hitTree(final String slope, int j) {
+        if (j >= slope.length()) {
             j = j % slope.length();
         }
         return tree == slope.charAt(j);
